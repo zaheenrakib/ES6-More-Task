877570ade09c339f9ddedd49b689da0eb9189ba2
Zaheen Rakib <rakibzaheen@gmail.com>
2024-02-23 17:11:33 +0600

    First Problem Sloved

---
+++ task-one.js
@@ -0,0 +1,24 @@
+// task -1: console log the secondary school location of Sophia 
+let data = {
+    Sophia: {
+        id: 33,
+        study: [
+            {
+                primary:
+                    [
+                        { school_name: "ABC primary school" },
+                        { location: "Peters burg" }
+                    ]
+            },
+            {
+                secondary:
+                    [
+                        { school_name: "ABC secondary school" },
+                        { location: "St Lorence" }
+                    ]
+            },
+        ]
+    }
+}
+
+console.log(data.Sophia.study[1].secondary[1]);
