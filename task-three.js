a5a315550f4c81e1e080a6f42292854717bad761
Zaheen Rakib <rakibzaheen@gmail.com>
2024-02-23 17:24:47 +0600

    Completed Problem Three

---
+++ task-three.js
@@ -0,0 +1,28 @@
+// task-3 : access and then show habluder adda
+// show output Beginner
+let data2 = {
+    data:
+        [
+            {
+                bookId: 1,
+                bookDetails: {
+                    name: "habluder adda",
+                    category: "XYZ",
+                    price: "20$",
+                },
+                bookCategory: "Basic",
+            },
+            {
+                bookId: 2,
+                bookDetails: {
+                    name: "gobluder adda",
+                    category: "ABC",
+                    price: "40$",
+                },
+                bookCategory: "Beginner",
+            }
+        ]
+}
+
+console.log(data2.data[0].bookDetails.name);
+console.log(data2.data[1].bookCategory);
