d7105e5990491742522be53de7ad3a71a200f5d2
Zaheen Rakib <rakibzaheen@gmail.com>
2024-02-23 17:19:22 +0600

    Second Problem Completed

---
+++ task-two.js
@@ -0,0 +1,29 @@
+// task-2: console .log  output: Petersburg, Herry
+
+let students = {
+    2222: {
+        name: "Jack",
+        section: "C",
+        class: "IX",
+        address: {
+            "building no": 12,
+            "street": "St. Jonson",
+            "city": "Petersburg",
+            "country": "UK"
+        }
+    },
+    3333: {
+        name: "Herry",
+        section: "D",
+        class: "X",
+        address: {
+            "building no": 85,
+            "street": "DC road",
+            "city": "Kachukhet",
+            "country": "Bangladesh"
+        }
+    }
+}
+
+console.log(students['2222'].address.city);
+console.log(students['3333'].name);
